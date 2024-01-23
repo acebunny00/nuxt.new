d03a2c8d01d72b1303635ef9ed76a448719d0fb1
DrKim <acebunny00@gmail.com>
2024-01-23 13:13:16 +0900

    ➕ Nuxt UI with Tailwind with Color mode
    https://ui.nuxt.com/getting-started/installation
    https://tailwindcss.com/docs/guides/nuxtjs
    https://color-mode.nuxtjs.org/#setup

---
+++ components/DemoNuxtUI.vue
@@ -0,0 +1,22 @@
+<template>
+	<UContainer>
+		<UCard class="mt-10">
+			<template #header>
+				<div class="flex justify-between">
+					<h1>Nuxt UI</h1>
+					<ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark', 'sepia']" /></ColorScheme>
+				</div>
+				<h2>with <a href="_tailwind/" target="_blank">Tailwindcss</a></h2>
+				<h2>with ColorMode</h2>
+			</template>
+		</UCard>
+	</UContainer>
+	<hr />
+</template>
+
+<script setup>
+	const colorMode = useColorMode();
+	console.log(colorMode.preference);
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+	<DemoNuxtUI />
+</template>
 
 <script setup></script>
 
