145f6d7902c7a90bdd9565e299a2bebe568e3577
DrKim <acebunny00@gmail.com>
2024-01-23 14:36:34 +0900

    🔧 Typescript & Telemetry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	typescript: { shim: false, strict: false },
+	telemetry: false,
 	devtools: {
 		enabled: true,
 		timeline: {
