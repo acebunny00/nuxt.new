e4c58779929d1e19474c6b500420c22d82d4fecd
DrKim <acebunny00@gmail.com>
2024-01-23 14:06:55 +0900

    ➕ Layout & Page & TimeLine with DevTools
    https://nuxt.com/docs/guide/directory-structure/layouts
    https://nuxt.com/docs/api/components/nuxt-layout
    
    https://nuxt.com/docs/guide/directory-structure/pages
    https://nuxt.com/docs/api/components/nuxt-page
    https://nuxt.com/docs/getting-started/routing
    
    https://devtools.nuxt.com/guide/getting-started

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h1>Nuxt Routing set up successfully!</h1>
+		<p>Current route: {{ route.path }}</p>
+		<a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+		<hr />
+	</div>
+	<slot />
+</template>
+
+<script setup>
+	const route = useRoute();
+</script>
+
+<style></style>
