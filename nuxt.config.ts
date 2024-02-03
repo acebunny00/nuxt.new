cce50914f3a1f23fca34a4086a127356a7b58b3b
DrKim <acebunny00@gmail.com>
2024-02-04 07:40:54 +0900

    🔧 SEO & Head & useRuntimeConfig
    https://nuxt.com/docs/guide/going-further/runtime-config
    https://nuxt.com/docs/getting-started/seo-meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true, timeline: { enabled: true } },
+	runtimeConfig: {
+		public: {
+			title: process.env.SEO_title,
+			description: process.env.SEO_description,
+			ogTitle: process.env.SEO_ogTitle,
+			ogDescription: process.env.SEO_ogDescription,
+			ogImage: process.env.SEO_ogImage,
+			ogUrl: process.env.SEO_ogUrl,
+			twitterTitle: process.env.SEO_twitterTitle,
+			twitterDescription: process.env.SEO_twitterDescription,
+			twitterImage: process.env.SEO_twitterImage,
+			twitterCard: process.env.SEO_twitterCard,
+			htmlAttrs_lang: process.env.SEO_htmlAttrs_lang,
+			link_rel: process.env.SEO_link_rel,
+			link_type: process.env.SEO_link_type,
+			link_href: process.env.SEO_link_href,
+		},
+	},
 	typescript: { shim: false, strict: false },
 	telemetry: false,
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,20 @@
 <template></template>
+<script setup>
+	const config = useRuntimeConfig();
+	useHead({
+		htmlAttrs: { lang: config.public.htmlAttrs_lang },
+		link: [{ rel: config.public.link_rel, type: config.public.link_type, href: config.public.link_href }],
+	});
+	useSeoMeta({
+		title: config.public.title,
+		description: config.public.description,
+		ogTitle: config.public.ogTitle,
+		ogDescription: config.public.ogDescription,
+		ogImage: config.public.ogImage,
+		ogUrl: config.public.ogUrl,
+		twitterTitle: config.public.twitterTitle,
+		twitterDescription: config.public.twitterDescription,
+		twitterImage: config.public.twitterImage,
+		twitterCard: config.public.twitterCard,
+	});
+</script>
